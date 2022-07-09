e59cc71bda09e4aa1f10db7575dd4f3d0f843671
Ramstain111si <82330389+Ramstain111si@users.noreply.github.com>
2022-07-10 00:29:56 +0300

    work finished, commit for user testing

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ $(document).ready(function(){
         $(".dropdown-content").slideToggle("slow");
     });
 });
+
+// textarea save as txt file function
+function saveTextAsFile() {
+    var textToWrite = document.getElementById("textToWrite").value;
+    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+    var fileNameToSaveAs = document.getElementById("fileNameToSaveAs").value;
+    var downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    downloadLink.click();
+}
+
+
